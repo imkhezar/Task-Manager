258e62070c4ec1c1e4c211f633433abcadbdef10
Muhammad Khezar <khezar_bangash2@hotmail.com>
2021-04-02 15:12:22 +0500

    delete by id on user and tasks done

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -68,6 +68,67 @@ app.get('/tasks/:id',async (req,res)=>{
 
     }
 })
+
+//update specific user by id
+app.patch('/users/:id', async (req,res)=>{
+    const update=Object.keys(req.body)
+    const allowedUpdates=['name','email','password','age']
+    const isValidOperation =update.every((update)=>{
+        return allowedUpdates.includes(update)
+    })
+
+    if(!isValidOperation){
+        return res.status(400).send({error:'invalide Updates'})
+    }
+    try{
+        const user =await User.findByIdAndUpdate(req.params.id,req.body,{new:true, runValidators:true})
+        
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+
+    }catch(e){
+        res.status(404).send(e)
+    }
+})
+
+//update specif task by id
+app.patch('/tasks/:id', async (req,res)=>{
+    const update=Object.keys(req.body)
+    const allowedUpdates=['descriptions','completed']
+    const isValidOperation=update.every((update)=>{
+        return allowedUpdates.includes(update)
+    })
+    if(!isValidOperation){
+        return res.status(400).send({error:"invalide updates on tasks"})
+    }
+
+    try{
+        const task= await Tasks.findByIdAndUpdate(req.params.id,req.body,{new:true, runValidators:true})
+        if(!task){
+            return res.status(400).send({error:'No Task Found'})
+        }
+         res.status(200).send('sucessfully updated')
+    }catch(e)
+    {
+        res.status(404).send(e)
+    }
+})
+
+//Delete User by ID
+app.delete('/users/:id',async (req,res)=>{
+    try{
+        const user= await User.findByIdAndDelete(req.params.id)
+        if(!user){
+            return res.status(404).send('No User Found')
+        }
+        res.send(user)
+    }catch(e){  
+        res.status(500).send()
+    }
+})
+
 app.post('/tasks', async (req,res)=>{
 
     const task = new Tasks(req.body)

e7fe3aa529ef7cc055676067e14eab87484bac15
Muhammad Khezar <khezar_bangash2@hotmail.com>
2021-04-26 03:25:04 +0500

    fetched data to react app

--- task-manager/public/front-end/src/App.js
+++ task-manager/public/front-end/src/App.js
@@ -4,15 +4,35 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import {React,Component} from 'react'
 
 
+
+//how to fetch data from express api in react
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+}
+
+callAPI() {
+    fetch("http://localhost:3001/users")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() {
+    this.callAPI();
+}
+
   render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Task Manager</NavbarBrand>
+            
+            
           </div>
         </Navbar>
+        <p>{this.state.apiResponse}</p>
       </div>
     );
   }

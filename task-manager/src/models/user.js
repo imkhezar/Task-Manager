3059a9191004b07eb39380f5cd1bc757be5e5e32
Muhammad Khezar <khezar_bangash2@hotmail.com>
2021-06-03 08:35:44 +0500

    cascading deleted users

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose =require('mongoose')
 const validator =require('validator')
 const bcrypt= require('bcryptjs')
 const jwt = require ('jsonwebtoken')
+const Task = require('./task')
 
 
 const userSchema = new mongoose.Schema({ 
@@ -101,6 +102,14 @@ userSchema.pre('save', async function(next){
         user.password=await bcrypt.hash(user.password,8)
     }
     next()
+})
+
+//Delet user tasks when user is removed
+userSchema.pre('remove',async function(next){
+    const user = this
+    await Task.deleteMany({owner:user._id})
+    next()
+
 })
 const User = mongoose.model('User',userSchema)
 
